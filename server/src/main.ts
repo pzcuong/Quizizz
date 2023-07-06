aad72ea2b86a0477c116b0847584d654c04313aa
Ngo Gia Phong <ngogiaphong123@gmail.com>
2023-07-06 20:25:22 +0700

    Exception handle for validation added

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { HttpException, HttpStatus, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
@@ -9,6 +9,14 @@ async function bootstrap() {
     app.useGlobalPipes(
         new ValidationPipe({
             whitelist: true,
+            exceptionFactory(errors) {
+                const message = errors
+                    .map((error) => {
+                        return Object.values(error.constraints);
+                    })
+                    .join(', ');
+                throw new HttpException(message, HttpStatus.BAD_REQUEST);
+            },
         }),
     );
     await app.listen(8080);
