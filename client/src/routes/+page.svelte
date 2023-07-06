29783e766e5efc64bb9099ee3403b8704a38fd35
PandoraLH <lixhen2002@gmail.com>
2023-07-06 16:23:10 +0700

    complete landingPage

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,7 +1,12 @@
-<h1 class="text-3xl font-bold underline">Hello world!</h1>
+<script>
+	import LandingPage from "../components/landingPage/landingPage.svelte";
+</script>
+
+<LandingPage/>
 
 <style lang="postcss">
 	:global(html) {
 		background-color: theme(colors.gray.100);
 	}
+	
 </style>
